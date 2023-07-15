6ffa87aa4835d30b71f8c0ec7efbe01e48438c47
Piyaporn259 <6310210259@psu.ac.th>
2023-07-15 17:59:05 +0700

    test

---
+++ project-spyder.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 15 17:20:54 2023
+
+@author: ComSci
+"""
+
+import tensorflow as tf
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
+
+#Build the model
+
+inputs = tf.keras.layer
